d5a0c05e1bb35850b376c42a35a1fced3d7786c7
Robert DeLaurentis <5385825+bobdel@users.noreply.github.com>
2020-05-26 16:15:17 -0700

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Robert DeLaurentis on 5/26/20.
+//  Copyright © 2020 Robert DeLaurentis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
