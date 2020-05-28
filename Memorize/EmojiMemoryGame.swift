40da38329aee376f1078faf988db32e4606d17e8
Robert DeLaurentis <5385825+bobdel@users.noreply.github.com>
2020-05-27 18:13:00 -0700

    Rather than using set on the entire class, just provide a var that lets views read the model.

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -14,9 +14,16 @@ class EmojiMemoryGame {
     // This is a handle to the model.
     // Explicit name used for instructional purposes. Better would be game or memoryGame.
     // The paramaterized type tells the model that this game uses strings.
-    // private(set) allows objects to "see" the model directly,
-    // but only EmojiMemoryGame can modify the model. It is a glass door.
-    private(set) var model: MemoryGame<String>
+    // private closes off objects to "see" the model directly
+    private var model: MemoryGame<String>
+
+    // MARK: -  Access the Model
+
+    // instead of using set on this class, read the var
+    // present the cards in a form more easily consumable by the view
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
 
     // MARK: -  Intent(s)
     // functions that allow views to access the model. Essentially openings in the glass door.
