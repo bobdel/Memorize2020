4d92ba02cf95e5e26e76081efe22f6ac1d351452
Robert DeLaurentis <5385825+bobdel@users.noreply.github.com>
2020-06-05 15:14:05 -0700

    Fixed match selection animations by changing the technique from if then of views to changing opacity so that when the second card is matched the animation appears while the card is revealed.

--- Memorize/Support/Cardify.swift
+++ Memorize/Support/Cardify.swift
@@ -27,13 +27,14 @@ struct Cardify: AnimatableModifier {
 
     func body(content: Content) -> some View {
         ZStack {
-            if isFaceUp {
+            Group {
                 RoundedRectangle(cornerRadius: self.cornerRadius).fill(Color.white)
                 RoundedRectangle(cornerRadius: self.cornerRadius).stroke(lineWidth: self.edgeLineWidth )
                 content // the view that is cardified
-            } else {
-                RoundedRectangle(cornerRadius: self.cornerRadius).fill()
-            }
+            }.opacity(isFaceUp ? 1 : 0)
+            
+            RoundedRectangle(cornerRadius: self.cornerRadius).fill().opacity(isFaceUp ? 0 : 1)
+
         }
         .rotation3DEffect(Angle.degrees(rotation), axis: (0,1,0))
     }
